4ab2b10b4d6616850b753365267e88f919a990d0
Muhammad Aminudin <aminudin199809@gmail.com>
2021-12-11 00:08:50 +0700

    Latihan 2 Js
    Mohon untuk dicek kembali ,Terimakasih

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var no = 4;
+var alamat = "Jl.Delima Jaya";
+var yakin = true;
+var norumah = null;
+var sebelah = [2, 4, 6, 8];
+
+console.log(no);
+console.log(alamat);
+console.log(yakin);
+console.log(norumah);
+console.log(sebelah);
